c793e153bdcf7424c70414fae857bb19e7d77ab2
Ludwig Geistlinger <Ludwig.Geistlinger@bio.ifi.lmu.de>
2016-01-13 13:41:15 +0000

    commit v2.0.5
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/EnrichmentBrowser@112511 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mapIds.R
+++ R/mapIds.R
@@ -9,7 +9,7 @@
 
 id.types <- function(org)
 {
-     org.pkg <- .org2pkg(org)
+    org.pkg <- .org2pkg(org)
     .isAvailable(org.pkg)
     org.pkg <- get(org.pkg) 
     return(keytypes(org.pkg))
@@ -23,8 +23,20 @@ map.ids <- function(eset, org=NA, from="ENSEMBL", to="ENTREZID")
     .isAvailable(org.pkg)
     org.pkg <- get(org.pkg) 
     x <- mapIds(org.pkg, keys=featureNames(eset), keytype=from, column=to)
-    x <- x[!is.na(x)]
-    x <- x[!duplicated(x)]
+    nr.na <- sum(is.na(x))
+    if(nr.na)
+    { 
+        message(paste("Excluded", nr.na, "genes without a corresponding to.ID"))
+        x <- x[!is.na(x)]
+    }
+    nr.dupl <- sum(table(x) > 1)
+    if(nr.dupl)
+    { 
+        message(paste("Encountered", nr.dupl, 
+            "from.IDs with >1 corresponding to.ID", 
+            "(a single to.ID was chosen for each of them)"))
+        x <- x[!duplicated(x)]
+    }
     eset <- eset[featureNames(eset) %in% names(x), ]
     names(x) <- NULL
     featureNames(eset) <- x
